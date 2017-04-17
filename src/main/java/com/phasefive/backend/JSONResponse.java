d91ce7ef2d6924351ae545d51db3a6e304abc700
Dylan Brown <Dylan Brown>
2017-04-17 15:58:38 -0400

    working on it

--- src/main/java/com/phasefive/backend/JSONResponse.java
+++ src/main/java/com/phasefive/backend/JSONResponse.java
@@ -45,9 +45,8 @@ public class JSONResponse extends SQLConnect {
 
 	public void obtainJSONResponse() throws JSONException, SQLException {
 		setUpJSONResponseFields();
-		if (!sc.getEmptyMonarchDB()) {
-			createJSONResponseArray();
-		}
+		createJSONResponseArray();
+
 		this.sendResponseToServlet(Phase5RESTController.out);
 	}
 
@@ -59,22 +58,14 @@ public class JSONResponse extends SQLConnect {
 
 	protected void createJSONResponseArray() {
 		try {
-			putMonarchJSONArrayIntoResponseArray();
-			putCreateJSONArrayIntoResponseArray();
-			putValidateJSONArrayIntoResponseArray();
-			putVerifyJSONArrayIntoResponseArray();
-			putSourceJSONArrayIntoResponseArray();
-			putCancelJSONArrayIntoResponseArray();
-			putReleaseJSONArrayIntoResponseArray();
-			putCompleteJSONArrayIntoResponseArray();
-			putMonarchOrderStatusCodesIntoResponseArray();
+			putJSONArrayIntoResponseArray();
 		} catch (JSONException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	protected void putMonarchJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray monarchSQLData = converter.convert(SQLConnect.getMonarchDBResults());
+	protected void putJSONArrayIntoResponseArray() throws JSONException, SQLException {
+		JSONArray SQLData = converter.convert(SQLConnect.getMonarchDBResults());
 		responseArray.put(0, monarchSQLData);
 	}
 
@@ -82,47 +73,6 @@ public class JSONResponse extends SQLConnect {
 		JSONArray createSQLData = converter.convert(SQLConnect.getCreateDBResults());
 		createSQLData.put(converter.convertEntityStatus(SQLConnect.getCreateDBResultsStatus(), SQLConnect.getCreateDBResultsStatusJobIDZero()));
 		responseArray.put(1, createSQLData);
-	}
-
-	protected void putValidateJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray validateSQLData = converter.convert(SQLConnect.getValidateDBResults());
-		validateSQLData.put(converter.convertEntityStatus(SQLConnect.getValidateDBResultsStatus(), SQLConnect.getValidateDBResultsStatusJobIDZero()));
-		responseArray.put(2, validateSQLData);
-	}
-
-	protected void putVerifyJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray verifySQLData = converter.convert(SQLConnect.getVerifyDBResults());
-		verifySQLData.put(converter.convertEntityStatus(SQLConnect.getVerifyDBResultsStatus(), SQLConnect.getVerifyDBResultsStatusJobIDZero()));
-		responseArray.put(3, verifySQLData);
-	}
-
-	protected void putSourceJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray sourceSQLData = converter.convert(SQLConnect.getSourceDBResults());
-		sourceSQLData.put(converter.convertEntityStatus(SQLConnect.getSourceDBResultsStatus(), SQLConnect.getSourceDBResultsStatusJobIDZero()));
-		responseArray.put(4, sourceSQLData);
-	}
-
-	protected void putCancelJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray cancelSQLData = converter.convert(SQLConnect.getCancelDBResults());
-		cancelSQLData.put(converter.convertEntityStatus(SQLConnect.getCancelDBResultsStatus(), SQLConnect.getCancelDBResultsStatusJobIDZero()));
-		responseArray.put(5, cancelSQLData);
-	}
-
-	protected void putReleaseJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray releaseSQLData = converter.convert(SQLConnect.getReleaseDBResults());
-		releaseSQLData.put(converter.convertEntityStatus(SQLConnect.getReleaseDBResultsStatus(), SQLConnect.getReleaseDBResultsStatusJobIDZero()));
-		responseArray.put(6, releaseSQLData);
-	}
-
-	protected void putCompleteJSONArrayIntoResponseArray() throws JSONException, SQLException {
-		JSONArray completeSQLData = converter.convert(SQLConnect.getCompleteDBResults());
-		completeSQLData.put(converter.convertEntityStatus(SQLConnect.getCompleteDBResultsStatus(), SQLConnect.getCompleteDBResultsStatusJobIDZero()));
-		responseArray.put(7, completeSQLData);
-	}
-	
-	protected void putMonarchOrderStatusCodesIntoResponseArray() throws JSONException, SQLException {
-		JSONArray monarchOrderStatusCodes = converter.convert(SQLConnect.getOMSCommonDBResults());
-		responseArray.put(8, monarchOrderStatusCodes);
 	}
 
 	protected void sendResponseToServlet(PrintWriter responseOutput) {
